c822863e10b3bbbc8217645fc29f0422d8ddb748
chenwenhua@juqitech.com <chenwenhua@juqitech.com>
2017-12-29 15:23:21 +0800

    测试接口转发

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+import axios from 'axios';
 class App extends Component {
+  constructor(...args){
+    super(...args)
+  }
+  http(){
+    axios({
+        method: 'get',
+        url: '/api/showapi/pub/site/1001/active_show?offset=0&length=10&type=2&src=m_web&sorting=weight&seq=desc&client=piaodashi_weixin&time=1514531694129&locationCityOID=3101&siteCityOID=1001'
+    }).then(function(response){
+       console.log(response)
+    }).catch(function(err){
+        console.log(err)
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +28,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.http.bind()}>接口</button>
       </div>
     );
   }
