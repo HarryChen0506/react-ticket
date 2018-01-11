9d573bcbb2e6ab0893047727424871066231f1ed
chenwenhua@juqitech.com <chenwenhua@juqitech.com>
2018-01-11 10:27:15 +0800

    引入fastclick

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import initReactFastclick from 'react-fastclick';
 
 //注入store
 import { Provider } from 'react-redux';
 import store from 'redux_module/store.js'
+
+initReactFastclick(); //fastclick在组件之前初始化
 
 ReactDOM.render((
     <Provider store={store}>
